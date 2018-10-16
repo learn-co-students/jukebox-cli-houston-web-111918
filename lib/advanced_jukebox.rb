8ee97b8a2824b42dc1c0f740907ac87da4345e42
Austin <Austin_MacBook@Austins-MacBook-Pro.local>
2018-10-15 19:10:00 -0500

    Done.

--- lib/advanced_jukebox.rb
+++ lib/advanced_jukebox.rb
@@ -12,9 +12,23 @@
 # "Graduation Failed" => '< path to this directory >/jukebox-cli/audio/Emerald-Park/07.mp3'
 # }
 
-def help
-  #this method should be the same as in jukebox.rb
+my_songs = {
+"Go Go GO" => '/Users/Austin_MacBook/Development/Flatiron-Github/jukebox-cli-houston-web-111918/audio/Emerald-Park/01.mp3',
+"LiberTeens" => '/Users/Austin_MacBook/Development/Flatiron-Github/jukebox-cli-houston-web-111918/audio/Emerald-Park/02.mp3',
+"Hamburg" =>  '/Users/Austin_MacBook/Development/Flatiron-Github/jukebox-cli-houston-web-111918/audio/Emerald-Park/03.mp3',
+"Guiding Light" => '/Users/Austin_MacBook/Development/Flatiron-Github/jukebox-cli-houston-web-111918/audio/Emerald-Park/04.mp3',
+"Wolf" => '/Users/Austin_MacBook/Development/Flatiron-Github/jukebox-cli-houston-web-111918/audio/Emerald-Park/05.mp3',
+"Blue" => '/Users/Austin_MacBook/Development/Flatiron-Github/jukebox-cli-houston-web-111918/audio/Emerald-Park/06.mp3',
+"Graduation Failed" => '/Users/Austin_MacBook/Development/Flatiron-Github/jukebox-cli-houston-web-111918/audio/Emerald-Park/07.mp3'
+}
 
+
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
 end
 
 
@@ -22,6 +36,7 @@ end
 def list(my_songs)
   #this method is different! Collect the keys of the my_songs hash and 
   #list the songs by name
+  my_songs.each_with_index {|index, num| puts "#{num + 1}: #{index}"}
 end
 
 
@@ -33,7 +48,19 @@ def play(my_songs)
   #if it isn't, tell them their choice is invalid
   #if it is, play the song using the system 'open <file path>' syntax
   #get the file path of the song by looking it up in the my_songs hash
+  puts "Please enter a song name or number:"
+  song = gets.chomp
   
+  if my_songs.include?(song)
+    puts "Playing #{song}"
+  else
+    song_num = song.to_i
+    if song_num < 1 || song_num > my_songs.length
+      puts "Invalid input, please try again"
+    else
+      puts "Playing #{my_songs[song_num - 1]}"
+    end
+  end
 end
 
 def exit_jukebox
